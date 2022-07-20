907807d46346343b7a7497160a29c5524eb2beb9
shb8204@naver.com <shb8204@naver.com>
2022-07-20 13:09:34 +0900

    webhook 바꾸고 재커밋

--- src/VisNetwork/VisNetwork2.jsx
+++ src/VisNetwork/VisNetwork2.jsx
@@ -91,6 +91,7 @@ const VisNetwork2 = ({ data }) => {
         <div>No Data</div>
         <div>2022년7월20일 테스트1</div>
         <div>2022년7월20일 테스트2</div>
+        <div>2022년7월20일 테스트3</div>
       </div>
     );
   }
