c1c4c4603bd12b5256c7d2c4da3885aa52208d10
shb8204@naver.com <shb8204@naver.com>
2022-07-25 17:20:25 +0900

    test 4

--- src/VisNetwork/VisNetwork2.jsx
+++ src/VisNetwork/VisNetwork2.jsx
@@ -92,6 +92,7 @@ const VisNetwork2 = ({ data }) => {
         <div>20220725 테스트1</div>
         <div>20220725 테스트2</div>
         <div>20220725 테스트3</div>
+        <div>20220725 테스트4</div>
       </div>
     );
   }
